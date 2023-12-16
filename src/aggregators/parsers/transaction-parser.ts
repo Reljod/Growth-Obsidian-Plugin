e11dd9e7a87dce7022f8af529c3c91e20a55ef37
Reljod Oreta <oretareljod@gmail.com>
2023-12-16 23:34:02 +0800

    Add details about the what NewLineSeparatedTransactionParser parses

--- src/aggregators/parsers/transaction-parser.ts
+++ src/aggregators/parsers/transaction-parser.ts
@@ -2,6 +2,20 @@ import { getAllLinesFromMarkdownContent } from "src/utils/helpers";
 import type { Transaction } from "../transactions-aggregator";
 
 export class NewLineSeparatedTransactionParser {
+	/*
+	Converts new line separated raw transaction string into a Transaction object.
+
+	Example of New Line Separated Raw Transaction string:
+	
+	#growth <-- tag
+	2023-12-16 <-- date
+	11:08 <-- time
+	gain <-- type
+	salary <-- details
+	20000 <-- value
+	From work <-- remarks
+
+	*/
 	parse(rawTransaction: string): Transaction {
 		const txn = getAllLinesFromMarkdownContent(rawTransaction);
 
